e29526b85a47bd867d011da29cb89d0ce615c307
Caleb Linden <48081625+calebcuthbertlinden@users.noreply.github.com>
2022-02-13 23:49:37 +0200

    Valentines day website

---
+++ valentinesday/src/App.js
@@ -0,0 +1,21 @@
+import logo from './heart.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <div id="wrapper">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            To my valentine, Stink Moose. I love you
+          </p>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
